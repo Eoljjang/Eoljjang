b653aa2786b4182f085473b44b5f5567a47156e6
Nathan Wong <nathan.wong2002@gmail.com>
2024-01-18 13:52:22 -0700

    about page (1/2)

--- profilePage/main.js
+++ profilePage/main.js
@@ -2,6 +2,11 @@ const hamburger_icon = document.querySelector(".hamburger-icon");
 const link_items = document.querySelectorAll(".link-item");
 const btn_resume = document.querySelector("#btn-resume");
 const btn_contact = document.querySelector("#btn-contact");
+const arrow_icon = document.querySelector("#arrow-icon");
+
+// JS To-Do (1)
+// - Make it so that all arrows when you click them, will go to the next section.
+// - You'll have to loop through all of them and add the appropriate "location.href=./#___".
 
 // Give all link-items an onclick.
 link_items.forEach(i => i.addEventListener("click", () =>{
